b1e34922d44f150c01eba49e5a05a88448657080
Zhu Jin Win10_Work <zhu444250169@gmail.com>
2019-08-20 17:06:20 +0800

    3.找出含有ATypeName的代码段，将其改写成完整的程序，然后编译，运行。

---
+++ do/src/unit2/Test03.java
@@ -0,0 +1,32 @@
+/**
+ * heyzen.club Inc.
+ * Copyright (c) 2018-2019 All Rights Reserved.
+ */
+package unit2;
+
+/**
+ * @author Zen
+ * @version 1.0: Test03.java, v 0.1 2019/08/20 16:46 Zen Exp $
+ */
+public class Test03 {
+    /**
+     * 3.找出含有ATypeName的代码段，将其改写成完整的程序，然后编译，运行。
+     * @param args
+     */
+    public static void main(String[] args) {
+        ATypeName a = new ATypeName();
+        System.out.println(a);
+    }
+}
+
+/**
+ * 这样写是不太好的，但是因为测试就这样简单的写吧
+ * 输出是：
+ * "C:\Program Files\Java\jdk1.7.0_80\bin\java.exe" "-javaagent:D:\CoderSoftware\Java\IntelliJ IDEA 2018.2.1\lib\idea_rt.jar=52341:D:\CoderSoftware\Java\IntelliJ IDEA 2018.2.1\bin" -Dfile.encoding=GBK -classpath "C:\Program Files\Java\jdk1.7.0_80\jre\lib\charsets.jar;C:\Program Files\Java\jdk1.7.0_80\jre\lib\deploy.jar;C:\Program Files\Java\jdk1.7.0_80\jre\lib\ext\access-bridge-64.jar;C:\Program Files\Java\jdk1.7.0_80\jre\lib\ext\dnsns.jar;C:\Program Files\Java\jdk1.7.0_80\jre\lib\ext\jaccess.jar;C:\Program Files\Java\jdk1.7.0_80\jre\lib\ext\localedata.jar;C:\Program Files\Java\jdk1.7.0_80\jre\lib\ext\sunec.jar;C:\Program Files\Java\jdk1.7.0_80\jre\lib\ext\sunjce_provider.jar;C:\Program Files\Java\jdk1.7.0_80\jre\lib\ext\sunmscapi.jar;C:\Program Files\Java\jdk1.7.0_80\jre\lib\ext\zipfs.jar;C:\Program Files\Java\jdk1.7.0_80\jre\lib\javaws.jar;C:\Program Files\Java\jdk1.7.0_80\jre\lib\jce.jar;C:\Program Files\Java\jdk1.7.0_80\jre\lib\jfr.jar;C:\Program Files\Java\jdk1.7.0_80\jre\lib\jfxrt.jar;C:\Program Files\Java\jdk1.7.0_80\jre\lib\jsse.jar;C:\Program Files\Java\jdk1.7.0_80\jre\lib\management-agent.jar;C:\Program Files\Java\jdk1.7.0_80\jre\lib\plugin.jar;C:\Program Files\Java\jdk1.7.0_80\jre\lib\resources.jar;C:\Program Files\Java\jdk1.7.0_80\jre\lib\rt.jar;E:\CodeFile\ThinkInJavaTest\out\production\do" unit2.Test03
+ * unit2.ATypeName@14a1ee92
+ *
+ * Process finished with exit code 0
+ */
+class ATypeName{
+
+}
